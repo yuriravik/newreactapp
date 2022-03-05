b3ee7e1bc4b1603993d488881152dc502a425694
Yuri Ravik <69877264+yuriravik@users.noreply.github.com>
2022-03-05 17:49:42 -0300

    new
    new

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https:www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Home/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
